79b26b1b3344710f8eb81b7ca14f770c3bdc3c64
Chyld Medford <chyld.medford@gmail.com>
2019-05-06 17:20:33 -0700

    wip

--- prompt.js
+++ prompt.js
@@ -13,5 +13,46 @@ rl.on("line", line => {
 });
 
 rl.on("close", () => {
-  console.log("x:", lines);
+  let local = "";
+  let upstream = "";
+  let ahead = 0;
+  let behind = 0;
+  let dirty = false;
+
+  lines.forEach(line => {
+    if (line[0] == "#") {
+      const sections = line.split(" ");
+
+      if (sections[1] == "branch.head") {
+        local = sections[2];
+      }
+
+      if (sections[1] == "branch.upstream") {
+        upstream = sections[2];
+      }
+
+      if (sections[1] == "branch.ab") {
+        ahead = parseInt(sections[2]);
+      }
+
+      if (sections[1] == "branch.ab") {
+        behind = Math.abs(parseInt(sections[3]));
+      }
+    } else {
+      dirty = true;
+    }
+  });
+
+  console.log(
+    "local:",
+    local,
+    "upstream:",
+    upstream,
+    "ahead:",
+    ahead,
+    "behind:",
+    behind,
+    "dirty:",
+    dirty
+  );
 });
