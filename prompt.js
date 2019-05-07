338d8f35a702f96b8830970fe34255eaf9da21f0
Chyld Medford <chyld.medford@gmail.com>
2019-05-06 17:03:43 -0700

    wip

--- prompt.js
+++ prompt.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+const lines = [];
+
+rl.on("line", line => {
+  lines.push(line);
+});
+
+rl.on("close", () => {
+  console.log("x:", lines);
+});
